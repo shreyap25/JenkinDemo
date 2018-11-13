3836c6fda682cef994c76e208e2adb2fbb187ef8
shreyap25 <shreya.pawar@capgemini.com>
2018-11-13 15:19:23 +0530

    File commited

---
+++ Jenkindemo/src/main/java/com/example/Jenkindemo/JenkindemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jenkindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkindemoApplication.class, args);
+		System.out.println("HELLO World...!!");
+	}
+}
